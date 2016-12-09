a6b206158c91f4cbec027b4406d3800109a67ca7
Pico Claudio <claudio-pico@hotmail.com>
2016-12-09 20:44:46 -0300

    terminado

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
-from flask import Flask, abort, make_response, request
+import flask
 
 import json
 
 import Procesos, Constantes
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 
 @app.route("/Processes")
@@ -25,10 +25,10 @@ def listarProceso(pid):
 
 @app.route('/Process', methods=['DELETE'])
 def matar():
-    if (not request.json or not 'pid' in request.json):
+    if (not flask.request.json or not 'pid' in flask.request.json):
         mensaje = {'Mensaje': Constantes.ingreseDatos}
         return response(mensaje, 400)
-    pid = int(request.json['pid'])
+    pid = int(flask.request.json['pid'])
     data = Procesos.matarProcesos(pid)
     if data == Constantes.noExistePorceso:
         mensaje = {'Mensaje': data, 'PID': pid}
@@ -39,34 +39,34 @@ def matar():
 
 @app.route("/Process", methods=['PUT'])
 def repriorizar():
-    if (not request.json or not 'NI' in request.json) or (not 'pid' in request.json):
+    if (not flask.request.json or not 'NI' in flask.request.json) or (not 'pid' in flask.request.json):
         mensaje = {'Mensaje': Constantes.ingreseDatos}
         return response(mensaje,400)
 
-    intNI = int(request.json['NI'])
-    pid = int(request.json['pid'])
+    intNI = int(flask.request.json['NI'])
+    pid = int(flask.request.json['pid'])
     if intNI < -20 or intNI > 20:
         mensaje = {'Mensaje': 'prioridades -20 hasta 20', 'PID': pid}
         return response(mensaje,400)
 
     data = Procesos.repriorizarProceso(pid, intNI)
     if data == Constantes.noExistePorceso:
-        mensaje = {'Mensaje': data, 'PID': request.json['pid']}
+        mensaje = {'Mensaje': data, 'PID': flask.request.json['pid']}
         return response(mensaje,404)
-    return response(data,400)
+    return response(data,200)
 
 
 @app.route("/Process", methods=['POST'])
 def lanzar():
-    if not request.json or not 'programa' in request.json:
+    if not flask.request.json or not 'programa' in flask.request.json:
         mensaje = {'Mensaje':Constantes.ingreseDatos}
         return response(mensaje,404)
-    data = Procesos.lanzarProcesos(request.json['programa'])
-    mensaje = {'Process': request.json['programa']}
+    data = Procesos.lanzarProcesos(flask.request.json['programa'])
+    mensaje = {'Process': flask.request.json['programa']}
     return response(mensaje,200)
 
 def response(data,code):
-    r = make_response(json.dumps(data),code)
+    r = flask.make_response(json.dumps(data), code)
     r.mimetype = 'application/json'
     return r
 
